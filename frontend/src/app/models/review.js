42bcf8e7fa1067d379bd2080e0293e9568f72cd2
jathwe <jathweems@gmail.com>
2021-04-07 12:10:06 -0500

    added Admin Dashboard and flagged Reviews

---
+++ frontend/src/app/models/review.js
@@ -0,0 +1,9 @@
+export class Review{
+    constructor(user,npo,rating,comment,date){
+        this.user=user;
+        this.npo=npo;
+        this.rating=rating;
+        this.comment=comment;
+        this.date=date;
+    }
+}
