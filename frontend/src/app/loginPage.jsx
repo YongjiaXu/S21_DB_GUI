df66c3fa1680e29b7cb05b0d7ccf0f511a783679
root <root@DESKTOP-18HED1E.localdomain>
2021-04-14 15:55:16 -0500

    saving before pulling from master

--- frontend/src/app/loginPage.jsx
+++ frontend/src/app/loginPage.jsx
@@ -2,66 +2,71 @@ import React from 'react';
 import {UserRepository} from '../api/userRepository';
 import {CreateAccount} from './createAccount';
 export class LoginPage extends React.Component {
-    userRepository = new UserRepository();
+    //userRepository = new UserRepository();
     state = {
         users : [],
         specific_user: ''
     }
 
-    login(user, password){
-        this.userRepository.getUser(user)
-            .then()
-    }
+    // login(user, password){
+    //     this.userRepository.getUser(user)
+    //         .then()
+    // }
 
     render () {
-        return <> 
-        <h1 id="login-header">Login</h1>
-        <div className="form-group">
-            <label htmlFor="name">Username:</label>
-            <br/>
-            <input type="text"
-                id="name"
-                name="name"
-                value={this.state.name}
-                onChange={event => this.setState({name: event.target.value})}
-                className="form-control" />
-        </div>
-        <div className="form-group">
-            <label htmlFor="email">Password:</label>
-            <br/>
-            <input type="text"
-                    id="email"
-                    name="email"
-                   value={this.state.email}
-                    onChange={event => this.setState({email: event.target.value})}
-                    className="form-control" />
-            </div>
-            <br/>
-            <button className="loginButton">Login</button>
-            <br/><br/>
-            <button className="newUser">Create New Account</button>
-        {/* <table>
-            <tbody>
-                {
-                    this.state.users.map((user, i) => <tr key={i}>
-                        <td> {user.username} </td>
-                        <td> {user.password} </td>
-                        <td> {user.user_type} </td>
-                    </tr>)
-                }
-            </tbody>
-        </table>
-            <p>single user: {this.state.specific_user}</p>
-            {console.log(this.state.specific_user)} */}
+        return <>
+                <div className="container float-right">
+                                <div className="row">
+                                    <div className="col-5 col-5 mx-auto">
+                                        <div className="card card-signin flex-row my-5">
+                                            <div className="card-body">
+                                                <div>
+                                                    <p className="grey-text text-darken-1">
+                                                        Create new account?
+                                                    </p>
+                                                </div>
+                                                <h5 className="card-title text-center">Login</h5>
+                                                <form className="form-signin" onSubmit={this.onSubmit}>
+                                                    <div className="form-label-group">
+                                                        <input
+                                                            onChange={this.onChange}
+                                                            value={this.state.email}
+                                                            id="email"
+                                                            type="email"
+                                                        />
+                                                        <label htmlFor="email">Email</label>
+                                                    </div>
+                                                        
+                                                    <div className="form-label-group">
+                                                        <input
+                                                            onChange={this.onChange}
+                                                            value={this.state.password}
+                                                            id="password"
+                                                            type="password"
+                                                        />
+                                                        <label htmlFor="password">Password</label>
+                                                    </div>
+                                                        
+                                                    <hr/>
+                                                        
+                                                        <button className="btn btn-lg btn-primary btn-block text-uppercase"
+                                                                type="submit">Login
+                                                        </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
         </>
     }
-    componentDidMount() {
-        this.userRepository.getUsers()
-            .then( users => this.setState({users: users}));
-        let username = 'npo';
-        this.userRepository.getUser(username)
-            .then( user => {
-                this.setState({specific_user: user[0].username});
-            });
-    }
+    // componentDidMount() {
+    //     this.userRepository.getUsers()
+    //         .then( users => this.setState({users: users}));
+    //     let username = 'npo';
+    //     this.userRepository.getUser(username)
+    //         .then( user => {
+    //             this.setState({specific_user: user[0].username});
+    //         });
+    // }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,4 +5,15 @@ import './index.css';
 import App from './app/app'
 import { LoginPage } from './app/loginPage';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default;
+      ReactDOM.render(NextApp);
+    });
+  }
