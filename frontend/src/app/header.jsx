cb48331c7b0b0b4aedf778d07c833f7227346250
root <root@DESKTOP-18HED1E.localdomain>
2021-04-25 15:15:46 -0500

    header change

--- frontend/src/app/header.jsx
+++ frontend/src/app/header.jsx
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom';
 export class Header extends React.Component{
     render(){
         return <>
-            <div class="card text-white bg-dark mb-3 rounded-0">
-                <div class="card-header text-right"><Link to={'/'}> Log Out </Link></div>
+            <div className="card text-white bg-dark mb-3 rounded-0">
+                <div className="card-header text-right"><Link to={'/'}> Log Out </Link></div>
             </div>
         </>
     }
